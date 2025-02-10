31ec80b0f744298d0c367f598ee48c6cb705d40d
alemar <alex92.marciuc@gmail.com>
2025-02-10 22:38:15 +0200

    update pom

--- src/test/java/alemar/TestNGTestRunner.java
+++ src/test/java/alemar/TestNGTestRunner.java
@@ -3,9 +3,11 @@ package alemar;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/cucumber",
+@CucumberOptions
+        (features = "src/test/java/cucumber",
         glue = "alemarStepDef",
-        plugin = {"pretty", "junit:target/cucumber-reports/cucumber-report.xml",
+        plugin = {"pretty",
+                "junit:target/cucumber-reports/cucumber-report.xml",
                 "json:target/cucumber-reports/cucumber-report.json",
                 "html:target/cucumber-reports/cucumber-report.html"},
         monochrome = true,
