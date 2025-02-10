8e94e14bc5a71e6c4cb6aea54eca755170d683f8
alemar <alex92.marciuc@gmail.com>
2025-02-10 21:59:09 +0200

    added scenario tag in test runner

--- src/test/java/alemar/TestNGTestRunner.java
+++ src/test/java/alemar/TestNGTestRunner.java
@@ -5,7 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/cucumber",
         glue = "alemarStepDef",
-        plugin = {"pretty", "json:target/cucumber-reports/cucumber-report.json", "html:target/cucumber-reports/cucumber-report.html"},
+        plugin = {"pretty", "junit:target/cucumber-reports/cucumber-report.xml",
+                "json:target/cucumber-reports/cucumber-report.json",
+                "html:target/cucumber-reports/cucumber-report.html"},
         monochrome = true,
         tags = "@credentials_env")
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
